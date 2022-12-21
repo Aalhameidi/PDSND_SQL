0c809508d1a08afa90e010fd0feb1de65d11e005
Aalhameidi <120863371+Aalhameidi@users.noreply.github.com>
2022-12-21 22:13:19 +0300

    Add files via upload

---
+++ sample.sql
@@ -0,0 +1,165 @@
+﻿WEBVTT
+
+1
+00:00:08.560 --> 00:00:10.700
+Regular physical activity can lower your
+
+2
+00:00:10.730 --> 00:00:16.320
+risk of heart and
+circulatory disease by 35%.
+
+3
+00:00:19.320 --> 00:00:21.600
+When you are active, the heat produced by
+
+4
+00:00:21.630 --> 00:00:27.180
+your muscles increases your body
+temperature, making you feel warmer.
+
+5
+00:00:27.210 --> 00:00:31.860
+Your heart starts to beat faster, pumping
+more blood to the muscles you're using.
+
+6
+00:00:31.890 --> 00:00:33.900
+Your heart is also a muscle.
+
+7
+00:00:33.930 --> 00:00:37.500
+If you are active regularly,
+it gets bigger and stronger.
+
+8
+00:00:37.530 --> 00:00:42.020
+Your muscles are working harder,
+so they need more oxygen.
+
+9
+00:00:42.050 --> 00:00:46.780
+You start to breathe faster so your blood
+can pick up more oxygen from your lungs.
+
+10
+00:00:46.810 --> 00:00:49.500
+Your lungs work harder
+to make this happen.
+
+11
+00:00:49.530 --> 00:00:53.260
+Once your blood has picked up oxygen,
+
+12
+00:00:53.290 --> 00:01:02.500
+it moves the muscles you are using,
+giving them the extra oxygen they need.
+
+13
+00:01:02.530 --> 00:01:04.770
+If you are active regularly, more
+
+14
+00:01:04.800 --> 00:01:07.740
+capillaries grow in the
+muscles you've been working.
+
+15
+00:01:07.770 --> 00:01:12.980
+This is one reason why activity
+starts to feel easier over time.
+
+16
+00:01:13.010 --> 00:01:15.980
+Getting active is great
+for people with diabetes.
+
+17
+00:01:16.010 --> 00:01:18.570
+If you have type two diabetes, you have
+
+18
+00:01:18.600 --> 00:01:23.100
+too much glucose in your blood, probably
+because you don't have enough insulin.
+
+19
+00:01:23.130 --> 00:01:26.700
+Physical activity helps you
+use the insulin you do have.
+
+20
+00:01:26.730 --> 00:01:31.290
+It also helps your cells use glucose
+even when there is no insulin.
+
+21
+00:01:31.320 --> 00:01:35.490
+Regular physical activity can improve
+your memory and attention span.
+
+22
+00:01:35.520 --> 00:01:37.820
+Over time, the bit of the brain involved
+
+23
+00:01:37.850 --> 00:01:42.280
+in memory and learning
+seems to get bigger.
+
+24
+00:01:44.360 --> 00:01:46.680
+Longterm physical activity leads to a
+
+25
+00:01:46.710 --> 00:01:49.660
+lower resting heart rate
+and lower blood pressure.
+
+26
+00:01:49.690 --> 00:01:53.930
+This helps cuts your risk of
+heart and circulatory disease.
+
+27
+00:01:53.960 --> 00:01:56.780
+And there are more reasons to smile.
+
+28
+00:01:56.810 --> 00:02:01.020
+When you're active, your brain
+produces chemicals called endorphins.
+
+29
+00:02:01.050 --> 00:02:04.290
+These reduce feelings of pain
+and make you feel more positive.
+
+30
+00:02:04.320 --> 00:02:08.610
+Getting active cuts down on stress
+hormones, reducing anxiety.
+
+31
+00:02:08.640 --> 00:02:11.380
+Combine activity with a balanced diet and
+
+32
+00:02:11.410 --> 00:02:14.340
+help yourself reach and
+maintain a healthy weight.
+
+33
+00:02:14.370 --> 00:02:18.980
+Aim for 150 minutes of moderate
+intensity activity per week.
+
+34
+00:02:19.010 --> 00:02:21.300
+Try to be active every day.
+
+35
+00:02:21.330 --> 00:02:22.760
+Every ten minutes counts.
+
